c1742aaa25996a277f7f2170e443cbe685bd309e
Kunal Shroff <shroffk@bnl.gov>
2016-01-29 16:07:19 -0500

    adding a prepare-release script for nsls2-product modules

---
+++ prepare-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Check parameters
+VERSION=$1
+COMPATLINK=$2
+MILESTONE=$3
+NOTES=$4
+PUSH=$5
+BUILD_DIR="build"
+if [ $# != 5 ]
+then 
+  echo You must provide the product version, compat link, milestone, notes \(e.g. \"prepare_release.sh 3.3.0 \"https://github\" \"https://github\" \"Some notes\"\"\)
+exit -1
+fi
+
+echo ::: Updating plugin versions ::
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.20.0:set-version -DnewVersion=$VERSION -Dartifacts=nsls2-product,products-csstudio-nsls2-features,org.csstudio.product.feature,org.csstudio.nsls2.product.feature,repository
+# update product because set-version doesn't
+sed -i 's/\(\<product[^>]\+\? version=\"\)[^"]*\("[^>]\+\?>\)/\1'${VERSION}'\2/g'  repository/css-nsls2.product
+
+echo ::: Committing and tagging version $VERSION :::
+git commit -a -m "Updating manifests to version $VERSION"
+if [ "$PUSH" = "true" ]
+then
+  echo ::: Tagging version $VERSION :::
+  git tag CSS-$VERSION
+  echo ::: Pushing changes :::
+  git push origin
+  git push origin CSS-$VERSION
+fi
